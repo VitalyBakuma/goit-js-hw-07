0064e31eebefabfe55740ba50ff4bfcecadab4ba
VitalyBakuma <prgbiwis@gmail.com>
2024-06-09 12:47:22 +0300

    thre

---
+++ src/js/task-1.js
@@ -0,0 +1,9 @@
+const categoriesList = document.querySelector('#categories');
+const items = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${items.length}`);
+items.forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const elements = item.querySelectorAll('ul li');
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elements.length}`);
+});
